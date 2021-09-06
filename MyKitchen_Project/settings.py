728e24700fec2d9434eaac9fba9c0412a0691248
suchitra2020180 <suchitraraniojha@gmail.com>
2021-09-06 11:44:31 +0530

    Created Contact Form

--- MyKitchen_Project/settings.py
+++ MyKitchen_Project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Mykitchen_App.apps.MykitchenAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Mykitchen_App/admin.py
+++ Mykitchen_App/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from Mykitchen_App.models import Contact
 # Register your models here.
+admin.site.register(Contact)
